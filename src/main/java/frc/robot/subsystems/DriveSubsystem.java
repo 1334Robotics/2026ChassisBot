02ea4f16c08ce9cd873261ac35576ba550804fec
Sadan (Master Mode) <KevinPlayz1970@gmail.com>
2025-12-01 16:39:07 -0500

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems;
 
-public class DriveSubsystem {
+import java.io.File;
 
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+
+import frc.robot.Constants;
+
+public class DriveSubsystem {
+    public DriveSubsystem() {
+        File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+        SwerveDrive  swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Constants.SwerveConstants.MAXIMUM_SPEED);
+
+    }
 }
