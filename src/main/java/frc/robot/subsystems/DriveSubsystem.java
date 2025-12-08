16d444017be0431f2298480e4e6a905dcc59828c
RyanMampilly <182558803+RyanMampilly@users.noreply.github.com>
2025-12-08 16:08:59 -0500

    Caught exception in DriveSubsystem and made class extend another

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,17 +1,21 @@
 package frc.robot.subsystems;
 
 import java.io.File;
+import java.io.IOException;
 
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
 import frc.robot.Constants;
 
-public class DriveSubsystem {
+public class DriveSubsystem extends SubsystemBase{
     public DriveSubsystem() {
         File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
-        SwerveDrive  swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Constants.SwerveConstants.MAXIMUM_SPEED);
-
+        try{
+            SwerveDrive  swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Constants.SwerveConstants.MAXIMUM_SPEED);
+        }
+        catch(IOException e) { throw new RuntimeException(e); }
     }
 }
