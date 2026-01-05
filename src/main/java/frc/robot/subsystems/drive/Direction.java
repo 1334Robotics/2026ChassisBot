9774d42606dcf0c2ad1e01d8137f5c580096121f
Yusuf Juman <yj.rex17@gmail.com>
2026-01-05 16:30:12 -0500

    added the swerve subsystem and direction.java
    Co-authored-by: kagjua <kagjua@users.noreply.github.com>
    Co-authored-by: Walter Li <walteryuli@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/drive/Direction.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.drive;
+
+public enum Direction {
+    FORWARDS(0),
+    LEFT(90),
+    RIGHT(270),
+    BACKWARDS(180);
+
+    public final int degrees;
+    Direction(int degrees) {
+        this.degrees = degrees;
+    }
+}
--- src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
@@ -1,15 +1,15 @@
 // Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
+// Open Source Softshare; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.drive;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public SwerveSubsystem() {}
 
   /**
    * Example command factory method.
@@ -21,7 +21,7 @@ public class ExampleSubsystem extends SubsystemBase {
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(
         () -> {
-          /* one-time action goes here */
+          /* one-time action goes here */ 
         });
   }
 
