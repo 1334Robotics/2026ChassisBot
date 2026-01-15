b7f9d5a09b891a6bd7b976b280e3f9feeb1cadee
Kelina Hu <1hukel@hdsb.ca>
2026-01-14 22:25:42 -0500

    Finished Swerve Program
    Introduced swerve drive functionality with new SwerveSubsystem, updated RobotContainer to use SwerveInputStream for field-oriented control, and added a deadband constant. Swerve configuration files were reorganized under a new 'swerve' directory. Added multiple vendor dependency JSON files for swerve and hardware support.

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -12,20 +12,28 @@ import java.io.File;
 import edu.wpi.first.wpilibj.Filesystem;
 import swervelib.parser.SwerveParser;
 import swervelib.SwerveDrive;
+import swervelib.SwerveInputStream;
+import java.util.function.Supplier;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 
 import static edu.wpi.first.units.Units.Meter;
 
 public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-
   File directory = new File(Filesystem.getDeployDirectory(),"swerve");
-  SwerveDrive  swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
-
+  SwerveDrive  swerveDrive; 
   public SwerveSubsystem() {
-        try
+    try
     {
-      swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.MAX_SPEED, startingPose);
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.maxSpeed, new Pose2d(
+                                                                                        new Translation2d(1.0,4.0),
+                                                                                        Rotation2d.fromDegrees(0)
+                                                                                      )
+                                                                  );                           
       // Alternative method if you don't want to supply the conversion factor via JSON files.
       // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
     } catch (Exception e)
@@ -37,7 +45,7 @@ public class SwerveSubsystem extends SubsystemBase {
   /**
    * Example command factory method.
    *
-   * @return a command 
+   * @return a command
    */
   public Command exampleMethodCommand() {
     // Inline construction of command goes here.
@@ -66,5 +74,19 @@ public class SwerveSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+
+  public SwerveDrive getSwerveDrive() {
+    return swerveDrive;
+  }
+
+  public void driveFieldOriented(ChassisSpeeds velocity) {
+    swerveDrive.driveFieldOriented(velocity);
+  }
+
+  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
+    return run(() -> {
+      swerveDrive.driveFieldOriented(velocity.get());;
+    });
   }
 }
